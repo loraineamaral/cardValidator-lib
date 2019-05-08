a93907ca5eac9690a3fd7cc90771e01c55072158
Loraine <loraineeamaral@gmail.com>
2019-05-08 13:29:42 -0300

    fix on spec

--- test/validCard.spec.js
+++ test/validCard.spec.js
@@ -4,21 +4,21 @@ let cardNumber = require("../lib/index");
 
 describe("cardValidator", () => {
   it("Testa cartão Inválido", () => {
-    expect(cardNumber.cardValidator("5292438341928601")).to.equal(false);
+    expect(cardNumber("5292438341928601")).to.equal(false);
   });
   it("Testa cartão Válido", () => {
-    expect(cardNumber.cardValidator("5292438341928602")).to.equal(true);
+    expect(cardNumber("5292438341928602")).to.equal(true);
   });
   it("Testa comprimento grande para cartão", () => {
-    expect(cardNumber.cardValidator("529243834192860201")).to.equal(false);
+    expect(cardNumber("529243834192860201")).to.equal(false);
   });
   it("Testa comprimento pequeno para cartão", () => {
-    expect(cardNumber.cardValidator("52924383419286")).to.equal(false);
+    expect(cardNumber("52924383419286")).to.equal(false);
   });
   it("Testa se números são todos iguais", () => {
-    expect(cardNumber.cardValidator("2222222222222222")).to.equal(false);
+    expect(cardNumber("2222222222222222")).to.equal(false);
   });
   it("Testa se cartão tem letras", () => {
-    expect(cardNumber.cardValidator("529243834192860X")).to.equal(false);
+    expect(cardNumber("529243834192860X")).to.equal(false);
   });
 });
